56774576bcb1c4c1490fa17480ad3010f419022e
WillyRumapea <willyrumapea123jr@gmail.com>
2024-11-10 22:36:17 +0700

    input warning

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+// const inputFrom = document.getElementById("input-from");
+// const warnFrom = document.getElementById("warn-from");
+
+// inputFrom.addEventListener("input", () => {
+//   if (inputFrom.value === "") {
+//     warnFrom.classList.add("hide-warn");
+//   } else {
+//     warnFrom.classList.remove("hide-warn");
+//   }
+// });
