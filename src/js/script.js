bcdaec58abd74c18f1101b7db011fc073f888fe9
WillyRumapea <willyrumapea123jr@gmail.com>
2024-11-13 22:34:44 +0700

    input nominal, button effect, login form

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,7 @@ const inputPrice = document.getElementById("input-price");
 priceButtons.forEach((priceButton) => {
   priceButton.addEventListener("click", (e) => {
     e.preventDefault();
+    priceButtons.forEach((btn) => btn.classList.remove("priceButtonClick"));
     priceButton.classList.add("priceButtonClick");
     const amount = priceButton.getAttribute("data-amount");
     inputPrice.value = amount;
