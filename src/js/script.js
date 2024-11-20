91e0e305bad88a0b66c5cec25e97dce77f2fae12
WillyRumapea <willyrumapea123jr@gmail.com>
2024-11-20 22:14:04 +0700

    moda pembayaran & input term

--- src/js/script.js
+++ src/js/script.js
@@ -2,14 +2,20 @@ const priceButtons = document.querySelectorAll(".price-button");
 const mediaButtons = document.querySelectorAll(".media-button");
 const inputPrice = document.getElementById("input-price");
 const loginButton = document.getElementById("login-button");
+const amountSupport = document.querySelector("#amount-support");
+const inputTerms = document.querySelectorAll(".term-condition");
+const payButtons = document.querySelectorAll(".payment-button");
 
 priceButtons.forEach((priceButton) => {
   priceButton.addEventListener("click", (e) => {
     e.preventDefault();
     priceButtons.forEach((btn) => btn.classList.remove("priceButtonClick"));
     priceButton.classList.add("priceButtonClick");
-    const amount = priceButton.getAttribute("data-amount");
+    const amount = Number(priceButton.getAttribute("data-amount"));
     inputPrice.value = amount;
+    amountSupport.textContent =
+      "Jumlah dukungan: " +
+      amount.toLocaleString("id-ID", { style: "currency", currency: "IDR" });
   });
 });
 
@@ -23,4 +29,37 @@ mediaButtons.forEach((mediaButton) => {
 
 loginButton.addEventListener("click", () => {
   document.location.href = "loginForm.html";
+});
+
+inputTerms.forEach((inputTerm) => {
+  inputTerm.addEventListener("change", (e) => {
+    payButtons.forEach((payButton) => {
+      if (e.target.checked) {
+        payButton.classList.remove("contrast-50");
+      } else {
+        payButton.classList.add("contrast-50");
+      }
+    });
+  });
+});
+
+payButtons.forEach((payButton) => {
+  payButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    payButtons.forEach((btn) => {
+      btn.classList.remove("buttonGateClick");
+      btn.classList.add("button-gate");
+    });
+
+    payButton.classList.add("button-gate");
+    payButton.classList.add("buttonGateClick");
+  });
+});
+
+window.addEventListener("load", () => {
+  const checkboxes = document.querySelectorAll(".term-condition");
+  checkboxes.forEach((checkbox) => {
+    checkbox.checked = false;
+  });
 });
